e0295ff988be8affb03d65abeb0305830819357c
glULTRA <7amakurd2002@gmail.com>
2022-09-24 10:19:30 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Create a frame.
+        JFrame window = new JFrame("Window");
+        // Frame properties.
+        window.setLayout(null);
+        window.setBounds(1920/3, 1080/5, 500, 500);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Let's make two list that we can intercet them.
+        // First set.
+        JTextField set1 = new JTextField("a,b,c");
+        set1.setBounds(50, 50, 200, 30);
+        window.add(set1);
+        // Second set.
+        JTextField set2 = new JTextField("b,c");
+        set2.setBounds(50, 100, 200, 30);
+        window.add(set2);
+
+        // Result label.
+        JLabel result = new JLabel("");
+        result.setBounds(50, 150, 500, 30);
+        window.add(result);
+
+        // Button of Interception.
+        JButton interception = new JButton("Interception");
+        interception.setBounds(300, 70, 130, 30);
+        interception.setBackground(Color.white);
+        window.add(interception);
+
+        // Action Listener of interception button.
+        interception.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String _set1 = set1.getText();
+                String _set2 = set2.getText();
+                result.setText(Sets.readableFormat(_set1) + " U " + Sets.readableFormat(_set2) + " = " + Sets.interception(_set1, _set2));
+            }
+        });
+
+        // Render the window.
+        window.setVisible(true);
+    }
+}
---
+++ Sets.java
@@ -0,0 +1,90 @@
+import java.util.Map;
+
+public class Sets 
+{
+    public static String interception(String set1, String set2){
+        // Format must be changed.
+        // return reFormat(set1).toString();
+        // return reFormat(set2).toString();
+        String [] set1_arr = reFormat(set1).split(",");
+        String [] set2_arr = reFormat(set2).split(",");
+
+        String set3 = "";
+
+        for (String el: set1_arr) {
+            for (String el2: set2_arr) {
+                if(el.equals(el2) &&  !isIn(el, set3)){
+                    set3 += el2 + ",";
+                }
+            }
+        }
+
+        set3 = readableFormat(set3);
+
+        return set3;
+    }
+
+    public static String readableFormat(String text){
+        if(text.isEmpty())
+            return "";
+            
+        if(text.toCharArray()[text.length()-1] == ','){
+            text = text.substring(0, text.length() - 1);
+        }
+        text = reFormat(text);
+        String result = "{ " + text + " }";
+
+        return result;
+
+    }
+
+    private static String reFormat(String text){
+        // Format incoming:
+        // 1- {a,b,c....d}
+        // 2- [a,b,c....d]
+        // 3- (a,b,c....d)
+        // 4- a,b,c...d
+        // 5- a b c d
+        // Formatting to -> a,b,c,d
+        // No spaces no commas no symbols!
+        Character[] rejectedChars = {
+            '{','}',',','(',')','[',']',' ',
+        };
+        String result = "";
+        String gatheringLetters = "";
+        for (Character letter : text.toCharArray()) {
+            if(letter == ','){
+                result += gatheringLetters + ",";
+                gatheringLetters = "";
+                continue;
+            }
+            if(!isIn(letter, rejectedChars)){
+                // System.out.println(letter);
+                gatheringLetters += letter;
+            }
+        }
+
+        result += gatheringLetters;
+
+        return result;
+    }
+
+    private static boolean isIn(Character c, Character[] list){
+        for (Character el : list) {
+            if(c == el)
+                return true;
+        }
+
+        return false;
+    }
+
+    private static boolean isIn(String item, String str){
+        String[] str_list = str.split(",");
+        for (String el : str_list) {
+            if(el.equals(item))
+                return true;
+        }
+
+        return false;
+    }
+}
